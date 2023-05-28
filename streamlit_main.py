dd1d03a9c0eb58d60a6b6f487175996a13fc29ed
Andjez <92543526+Andjez@users.noreply.github.com>
2023-05-28 23:45:49 +0530

    streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -24,7 +24,7 @@ from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 
 #embedding
-instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large", model_kwargs={"device": "cpu"})
+instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base", model_kwargs={"device": "cpu"})
 
 def extract_video_code(url):
     pattern = r"(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:embed\/|watch\?v=)|youtu\.be\/)([\w-]+)"
