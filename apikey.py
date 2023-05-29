15a979a58def59e0117e0e9db567513c391e759d
Andjez <92543526+Andjez@users.noreply.github.com>
2023-05-29 23:34:09 +0530

    apikey.py

--- apikey.py
+++ apikey.py
@@ -8,4 +8,4 @@
 # Copyright:   (c) Androse_Jes 2023
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
-openai_apikey = "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"
+openai_apikey = "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"
