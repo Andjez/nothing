6739169a4e64a9feb0f529607d0a5a4617c92b01
Andjez <92543526+Andjez@users.noreply.github.com>
2023-06-11 23:36:27 +0530

    streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -47,7 +47,7 @@ user_input = ""
 #instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base", model_kwargs={"device": "cpu"})
 col1,col2 = st.columns(2)
 with col1:
-    if st.button("Clear Database"):
+    if st.button("Clear All"):
     # Clears all st.cache_resource caches:
         st.cache_resource.clear()
 with col2:
@@ -56,10 +56,13 @@ with col2:
         st.session_state["past"] = []
         st.session_state["source"] = []
         st.session_state["time_sec"] = []
+        yt_link = ""
+        user_input = ""
 
 @st.cache_resource
-def load_chain(yt_link):
-    instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base", model_kwargs={"device": "cpu"})
+instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-base", model_kwargs={"device": "cpu"})
+
+def load_chain(yt_link,instructor_embeddings):
     loader = YoutubeLoader.from_youtube_url(yt_link, add_video_info=True)
     documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
@@ -69,11 +72,12 @@ def load_chain(yt_link):
     return retriever
 
 yt_link = st.text_input("enter youtube link here!")
+
 def get_text():
     input_text = st.text_input("You: ", "")
     return input_text
 if yt_link:
-    chain = load_chain(yt_link)
+    chain = load_chain(yt_link,instructor_embeddings)
     model = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
     user_input = get_text()
 
